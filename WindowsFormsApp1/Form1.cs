5347495e4e69ef63ef5906cf5dcda5595d4d4fc6
tkdfhr8744 <tkdfhr8744@naver.com>
2018-11-12 10:59:13 +0900

    button source

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,6 +15,32 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            Load += Form1_Load1;
         }
+
+        private void Form1_Load1(object sender, EventArgs e)
+        { 
+            Button button1 = new Button();
+
+            button1.DialogResult = DialogResult.OK;
+            button1.Text = "확인";
+            button1.Size = new Size(100, 50);
+            button1.Location = new Point(30, 30);
+
+            Controls.Add(button1);
+            button1.Click += button1_click;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_click(object o, EventArgs a)
+        {
+            MessageBox.Show("클릭 확인!");
+        }
+        
     }
 }
